5d1a3c62f04811b66762c95f0ceb6316f9cfee4d
Eric Tsai <chunchieh@gmail.com>
2016-06-08 07:36:37 +0000

    [what] addd class object for backtracking algorithms

--- backtracking.py
+++ backtracking.py
@@ -1,25 +1,27 @@
 
-def permute(nums, used, stack):
-    if len(nums) == len(stack):
+class Permutation(object):
+    def permute(self, nums, used, stack):
+        if len(nums) == len(stack):
+            print stack
+            return
+
+        for i, x in enumerate(nums):
+            if not used[i]:
+                used[i] = True
+                stack.append(x)
+                self.permute(nums, used, stack)
+                stack.pop()
+                used[i] = False
+
+class Combination(object):
+    def combine(self, nums, start, stack):
         print stack
-        return
 
-    for i, x in enumerate(nums):
-        if not used[i]:
-            used[i] = True
-            stack.append(x)
-            permute(nums, used, stack)
+        n = len(nums)
+        for i in xrange(start, n):
+            stack.append(nums[i])
+            self.combine(nums, i+1, stack)
             stack.pop()
-            used[i] = False
-
-def combine(nums, start, stack):
-    print stack
-
-    n = len(nums)
-    for i in xrange(start, n):
-        stack.append(nums[i])
-        combine(nums, i+1, stack)
-        stack.pop()
 
 if __name__ == '__main__':
     def test_permute():
@@ -27,12 +29,12 @@ if __name__ == '__main__':
         nums = range(4)
         n = len(nums)
         used = [False] * n
-        permute(nums, used, [])
+        Permutation().permute(nums, used, [])
 
     def test_combine():
         print '> test_combine'
         nums = range(4)
-        combine(nums, 0, [])
+        Combination().combine(nums, 0, [])
 
     def main():
         test_permute()
